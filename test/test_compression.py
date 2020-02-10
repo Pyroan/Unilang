40f35fa8d04883b0d2deb8e5c8be3c76c039f918
Evan Schoenberger <evanjschoenberger@gmail.com>
2020-02-09 17:56:09 -0800

    make a version of compression that actually works With the slight caveat that it compresses the string backwards

--- test/test_compression.py
+++ test/test_compression.py
@@ -1,8 +1,33 @@
 import unittest
 from src.compression import *
 
+@unittest.skip
+class TestConsistency(unittest.TestCase):
+    def test_consistency(self):
+        cases = []
+        for i in cases:
+            self.assertEqual(
+                decompress_program(compress_program(i)), i
+            )
+    
+    # Calling compression on a compressed string should do nothing
+    def test_multiple_compress(self):
+        cases = []
+        for i in cases:
+            self.assertEqual(
+                compress_program(i),
+                compress_program(compress_program(i))
+            )
+    
+    # Calling decompression on a decompressed string _really_ shouldn't do anything.
+    def test_multiple_decompress(self):
+        cases = []
+        for i in cases:
+            self.assertEqual(decompress_program(i), i)
+
+
 class TestCompression(unittest.TestCase):
-    def test_compressSnippet(self):
+    def test_compressProgram(self):
         cases = {
             'a': 'a',
             '``': '\u0080',
@@ -10,14 +35,14 @@ class TestCompression(unittest.TestCase):
             'aa': '\u00a1',
             '\u007f\u007f': '\u047f',
             '\u007f\u007f\u007f': '\u847f',
-            'abc': '\u08c3'
+            'abc': '\u08c3',
+
+            '345ab1vv3v': '345\u00a21\u03563v',
+            'aaaaa': 'a\U000108a1', # this is bad depending on our goals
+
         }
         for i, o in cases.items():
-            self.assertEqual(compress_snippet(i), o)
-
-    @unittest.skip
-    def test_compressProgram(self):
-        self.fail()
+            self.assertEqual(compress_program(i), o)
 
 class TestDecompression(unittest.TestCase):
     def test_decompressChar(self):
@@ -32,13 +57,6 @@ class TestDecompression(unittest.TestCase):
         cases = {}
         for i, o in cases.items():
             self.assertEqual(decompress_char(i), o)
-            
-
-    # Test if all codepoints from 0000 to FFFF are valid
-    # We wanna test codepoints past that eventually but heck it.
-    @unittest.skip
-    def test_decompressAllUnicode(self):
-        self.fail()
 
 
 if __name__ == '__main__':
